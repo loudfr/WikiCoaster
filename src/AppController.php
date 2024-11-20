d6cee5c16cae7f302c714f247d0cc8426ede53ef
loudfr <louli.dufour@gmail.com>
2024-11-20 12:00:18 +0100

    Premier commit

---
+++ src/AppController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AppController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        // Retourne le contenu de la vue 'app/index'
+        return $this->render('index.html.twig');
+    }
+}
