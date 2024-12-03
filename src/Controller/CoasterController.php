14432687e756d10130b05bf4ac2a0231505bd124
loudfr <louli.dufour@gmail.com>
2024-12-03 12:11:59 +0100

    affichage complet + bouton modifier

--- src/Controller/CoasterController.php
+++ src/Controller/CoasterController.php
@@ -5,28 +5,29 @@ namespace App\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Coaster;
 use App\Form\CoasterType;
 use App\Repository\CoasterRepository;
+use Doctrine\ORM\EntityManager;
 
 class CoasterController extends AbstractController
 {
     #[Route(path: '/coaster/add')]
-    public function add(EntityManagerInterface $em, Request $request): Response
+    public function add(EntityManagerInterface $entityManager, Request $request): Response
     {
         $entity = new Coaster();
         $form = $this->createForm(CoasterType::class, $entity);
 
         $coaster = new Coaster();
-        $coaster->setName('Blue Fire')
+        /*$coaster->setName('Blue Fire')
             ->setmaxHeight(38)
             ->setMaxSpeed(100)
             ->setLength(1050)
             ->setOperating(true) 
-        ;
+        ;*/
 
         $form = $this->createForm(CoasterType::class, $coaster);
 
@@ -36,10 +37,10 @@ class CoasterController extends AbstractController
         // ap
         if ($form->isSubmitted() && $form->isValid()) {
             // ajoute la nouvelle entité dans le manager Doctrine
-            $em->persist($coaster);
+            $entityManager->persist($coaster);
 
             // Met à jour la DB
-            $em->flush();
+            $entityManager->flush();
 
             return $this->redirectToRoute('app_coaster_index');
         }
@@ -64,9 +65,68 @@ class CoasterController extends AbstractController
     public function index(CoasterRepository $coasterRepository): Response
     {
         $coasters = $coasterRepository->findAll();
+
+        //dump($coasters);
+        
         return $this->render('coaster/index.html.twig', [
             'coasters' => $coasters,
         ]);
+    }
+
+    //récupérer un nom à partir d'un id et modifier
+    #[Route('/coaster/{id}/edit')]
+    public function edit(Coaster $coaster, Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        $form = $this->createForm(CoasterType::class, $coaster);
+        $form->handleRequest($request);
+
+        // ap
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            //maj bd
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_coaster_index');
+        }
+
+        return $this->render('coaster/edit.html.twig', [
+            'coasterForm' => $form,
+        ]);  
+
+        return new Response($coaster->getName());
+    }
+
+    //récupérer un nom à partir d'un id 
+    #[Route('/coaster/{id}/delete')]
+    public function delete(Coaster $coaster, Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        $form = $this->createForm(CoasterType::class, $coaster);
+        $form->handleRequest($request);
+
+        // ap
+        if ($form->isSubmitted() && $form->isValid()) {
+            //maj bd
+            $entityManager->flush();
+            return $this->redirectToRoute('app_coaster_index');
+        }
+
+        if ($this->isCsrfTokenValid(
+            'delete'.$coaster->getId(),
+            $request->request->get('_token')
+        )) {
+            $entityManager->remove($coaster);
+            $entityManager->flush();
+        
+            return $this->redirectToRoute('app_coaster_index');
+        }        
+
+        return $this->render('coaster/delete.html.twig', [
+            'coasterForm' => $form,
+        ]);  
+
+        return new Response($coaster->getName());
     }
 
 }
